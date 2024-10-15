b13f848bba64028ae0e41179cea4afdd71764dea
kushalkk752 <kushalkk555@gmail.com>
2024-10-15 23:49:32 +0530

    fixed the defect 1345 by adding login feature

---
+++ src/main/java/com/registration/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.registration.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    public static void main(String[] args) {
+        System.out.println("Login feature added");
+    }
+}
